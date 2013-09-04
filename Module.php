45420ec577474556e0f1d5520deeb5ed6feabdf9
David Pozsar <david.pozsar@megaweb.hu>
2013-09-04 16:15:24 +0200

    first commit

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Grid\Search;
+
+use Zork\Stdlib\ModuleAbstract;
+
+/**
+ * Grid\Search\Module
+ *
+ * @author David Pozsar <david.pozsar@megaweb.hu>
+ */
+class Module extends ModuleAbstract
+{
+
+    /**
+     * Module base-dir
+     *
+     * @const string
+     */
+    const BASE_DIR = __DIR__;
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Search module for Gridguyz
+==========================
+
+Search module provides functionality, to be able to search in contents.
+
+License
+-------
+
+Gridguyz is under **Creative Commons Attribution-ShareAlike 3.0** (cc-by-sa-3.0).
+More info at [LICENSE.txt](LICENSE.txt).
