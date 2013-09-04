3953af629c39f450b026daf3bc0c6f06b7fd43dd
David Pozsar <david.pozsar@megaweb.hu>
2013-09-04 16:31:21 +0200

    add search module to the module table

--- sql/grid-search/site/data.0-0.1.sql
+++ sql/grid-search/site/data.0-0.1.sql
@@ -1,3 +1,8 @@
+-- insert default values for table: module
+
+INSERT INTO "module" ( "module", "enabled" )
+     VALUES ( 'Grid\Search', TRUE );
+
 -- default values for plugins of table: search
 
 SELECT "table_plugin_run_create_triggers"( 'search' );
--- sql/grid-search/site/data.0.1-0.sql
+++ sql/grid-search/site/data.0.1-0.sql
@@ -1,3 +1,8 @@
+-- remove data
+
+DELETE FROM "module"
+      WHERE "module" = 'Grid\Search';
+
 -- delete default values for table: search
 
 SELECT "table_plugin_run_drop_triggers"( 'search' );
