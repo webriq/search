a410770397f7bf3c4930393a34407054fb0fc7ee
David Pozsar <david.pozsar@megaweb.hu>
2013-09-05 17:09:07 +0200

    search results now a meta-content

--- sql/grid-search/site/data.0-0.1.sql
+++ sql/grid-search/site/data.0-0.1.sql
@@ -3,6 +3,31 @@
 INSERT INTO "module" ( "module", "enabled" )
      VALUES ( 'Grid\Search', TRUE );
 
+-- insert search meta-contents: search.results
+
+DO LANGUAGE plpgsql $$
+DECLARE
+    "vLastId"   INTEGER;
+BEGIN
+
+    -- insert search.results
+
+    INSERT INTO "paragraph" ( "type", "left", "right", "name" )
+         VALUES ( 'metaContent', 1, 6, 'search.results' );
+
+    "vLastId" = currval( 'paragraph_id_seq' );
+
+    INSERT INTO "paragraph_property" ( "paragraphId", "locale", "name", "value" )
+         VALUES ( "vLastId", 'en', 'title', 'Search' );
+
+    INSERT INTO "paragraph" ( "type", "rootId", "left", "right", "name" )
+         VALUES ( 'title', "vLastId", 2, 3, NULL );
+
+    INSERT INTO "paragraph" ( "type", "rootId", "left", "right", "name" )
+         VALUES ( 'contentPlaceholder', "vLastId", 4, 5, NULL );
+
+END $$;
+
 -- default values for plugins of table: search
 
 SELECT "table_plugin_run_create_triggers"( 'search' );
--- sql/grid-search/site/data.0.1-0.sql
+++ sql/grid-search/site/data.0.1-0.sql
@@ -3,6 +3,12 @@
 DELETE FROM "module"
       WHERE "module" = 'Grid\Search';
 
+-- delete search meta-contents: search.results
+
+DELETE FROM "paragraph"
+      WHERE "type" = 'metaContent'
+        AND "name" = 'search.results';
+
 -- delete default values for table: search
 
 SELECT "table_plugin_run_drop_triggers"( 'search' );
