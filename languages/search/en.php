794b978181237595af4eac8fc99338b32c8a4022
David Pozsar <david.pozsar@megaweb>
2013-11-26 16:10:27 +0100

    search enhancements

--- languages/search/en.php
+++ languages/search/en.php
@@ -6,7 +6,7 @@ return array(
     'search.form.query' => 'Search query',
     'search.form.submit' => 'Search',
     'search.form.paragraph.displayInstallOpensearch' => 'Display install',
-    'search.form.paragraph.displayInstallOpensearch.description' => 'If browser supports <a href="http://www.opensearch.org/">opensearch</a> extensions (and this feature is enabled), an install button will be placed after the search button for the user, in order to be able to install this search engine for later use.',
+    'search.form.paragraph.displayInstallOpensearch.description' => 'If browser supports <a target="_blank" href="http://www.opensearch.org/">opensearch</a> extensions (and this feature is enabled), an install button will be placed after the search button for the user, in order to be able to install this search engine for later use.',
     'search.install.label' => '+',
     'search.install.title' => 'Install this search engine to your browser as a search plugin, to search this site at any time.',
     'search.results.title' => 'Search',
